e1d4cf1cc40fca810d20e80b6a840fd5f6ca3bbd
Rodrigo Vidal <rodrigovidal777@gmail.com>
2011-06-16 00:42:50 -0300

    Problem #1

---
+++ ProjectEuler.Tests/Euler1Tests.fs
@@ -0,0 +1,48 @@
+﻿module Tests
+open NUnit.Framework
+open FsxUnit
+open ProjectEuler
+
+type ``Project Euler 1 Tests``() = 
+    [<Test>] member test.
+        ``Given a empty list sum all members should be equal 0``() =
+            [] |> sumAllMultiplesOf3Or5 |> should be (equal 0)
+    [<Test>] member test.
+        ``Given a list with 1 sum all members should be equal 0``() =
+            [1] |> sumAllMultiplesOf3Or5 |> should be (equal 0)
+
+    [<Test>] member test.
+        ``Given a list with 2 sum all members should be equal 0``() =
+            [2] |> sumAllMultiplesOf3Or5 |> should be (equal 0)
+    
+    [<Test>] member test.
+        ``Given a list with 3 sum all members should be equal 3``() =
+            [3] |> sumAllMultiplesOf3Or5 |> should be (equal 3)
+    
+    [<Test>] member test.
+        ``Given a list with 4 sum all members should be equal 0``() =
+            [4] |> sumAllMultiplesOf3Or5 |> should be (equal 0)
+
+    [<Test>] member test.
+        ``Given a list with 5 sum all members should be equal 5``() =
+            [5] |> sumAllMultiplesOf3Or5 |> should be (equal 5)
+
+    [<Test>] member test.
+        ``Given a list with 1,2,3 sum all members``() =
+            [1;2;3] |> sumAllMultiplesOf3Or5 |> should be (equal 3)
+
+    [<Test>] member test.
+        ``Given a list with 1,2,3,4,5 sum all members``() =
+            [1;2;3;4;5] |> sumAllMultiplesOf3Or5 |> should be (equal 8)
+
+    [<Test>] member test.
+        ``Given a list with 1,2,3,4,5,6 sum all members``() =
+            [1;2;3;4;5;6] |> sumAllMultiplesOf3Or5 |> should be (equal 14)
+
+    [<Test>] member test.
+        ``Given a list with 1,2,3,4,5,6,7,8,9,10 sum all members``() =
+            [1;2;3;4;5;6;7;8;9;10] |> sumAllMultiplesOf3Or5 |> should be (equal 33)
+
+    [<Test>] member test.
+        ``Given a list with [1..999] sum all members``() =
+            [1..999] |> sumAllMultiplesOf3Or5 |> should be (equal 233168)
---
+++ ProjectEuler/Euler1.fs
@@ -0,0 +1,19 @@
+﻿module ProjectEuler
+
+let isMultipleOf3Or5 x = x % 3 = 0 || x % 5 = 0
+
+let sumAllMultiplesOf3Or5Ref list = 
+    list |> List.filter isMultipleOf3Or5 
+         |> List.sum
+            
+let isMultipleOf3Or5 x = x % 3 = 0 || x % 5 = 0
+
+let sumAllMultiplesOf3Or52 list = List.sum (List.filter isMultipleOf3Or5 list)
+
+let sumAllMultiplesOf3Or5 list = 
+    let mutable total = 0
+    for i in list do
+        if i % 3 = 0 || i % 5 = 0 then
+            total <- total + i
+    total
+         
