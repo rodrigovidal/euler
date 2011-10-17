c749b90fbf985a1fb09d4706c7b45a8f66fcec6d
Rodrigo Vidal <rodrigovidal777@gmail.com>
2011-10-17 03:36:16 -0300

    Euler2

--- ProjectEuler/Euler1.fs
+++ ProjectEuler/Euler1.fs
@@ -1,15 +1,9 @@
 ﻿module ProjectEuler
 
-let isMultipleOf3Or5 x = x % 3 = 0 || x % 5 = 0
-
-let sumAllMultiplesOf3Or5Ref list = 
-    list |> List.filter isMultipleOf3Or5 
+let sumAllMultiplesOf3Or5Fun list = 
+    list |> List.filter (fun x -> x % 3 = 0 || x % 5 = 0)
          |> List.sum
             
-let isMultipleOf3Or5 x = x % 3 = 0 || x % 5 = 0
-
-let sumAllMultiplesOf3Or52 list = List.sum (List.filter isMultipleOf3Or5 list)
-
 let sumAllMultiplesOf3Or5 list = 
     let mutable total = 0
     for i in list do
---
+++ ProjectEuler/Euler2.fs
@@ -0,0 +1,8 @@
+﻿module Euler2
+
+let fibonacci =
+    (0, 1) 
+    |> Seq.unfold(fun (current, next) -> Some(current, (next, current + next)))
+    |> Seq.takeWhile(fun x -> x <= 4000000)
+    |> Seq.filter(fun x -> x % 2 = 0)
+    |> Seq.sum
