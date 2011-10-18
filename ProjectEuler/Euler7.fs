1265384aedfa45b10ea5928cd744ecfc3f59c1d4
Rodrigo Vidal <rodrigovidal777@gmail.com>
2011-10-18 04:01:22 -0300

    Euler7

---
+++ ProjectEuler/Euler7.fs
@@ -0,0 +1,7 @@
+﻿module Euler7
+
+let isPrime n = 
+    let squareRoot = n |> float |> sqrt |> int64
+    { 2L .. squareRoot } |> Seq.forall(fun m -> n % m <> 0L)
+let primes = seq {1L .. System.Int64.MaxValue} |> Seq.filter(isPrime)
+let result = primes |> Seq.nth 10001
